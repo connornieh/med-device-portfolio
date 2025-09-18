54e171a2d9f1578a94e5cf4a757aaeb066f0b497
shreyasjd777 <shreyasjd777@gmail.com>
2025-09-18 15:09:27 -0700

    Finished Home, Header, and Footer

---
+++ src/components/Home.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="about d-flex align-items-center justify-content-between gap-2 p-4 rounded-5 shadow">
+    <img src="../assets/portrait.jpg" alt="Headshot" class="portrait rounded-4 border"></img>
+    <p class="text">
+      &nbsp;&nbsp;&nbsp;&nbsp;Hi! I'm Connor, a bioengineer with a B.S. in Bioengineering 
+      from UC Riverside, currently pursuing an M.Eng in Med Tech Design at Duke University. 
+      I am passionate about applying my strong background in biomedical research and expertise 
+      in the design process to create patient-driven innovations with impactful clinical 
+      applications. My goal is to contribute to a healthcare landscape that is not only more 
+      effective but also more inclusive, using technology as a tool to drive health equity 
+      and ensure that high-quality care is accessible to all.
+    </p>
+  </div>
+  <div class="about d-flex align-items-center justify-content-between gap-2 p-4 rounded-5 shadow">
+    <img src="../assets/portrait.jpg" alt="Headshot" class="portrait rounded-4 border"></img>
+    <p class="text">
+      &nbsp;&nbsp;&nbsp;&nbsp;Hi! I'm Connor, a bioengineer with a B.S. in Bioengineering 
+      from UC Riverside, currently pursuing an M.Eng in Med Tech Design at Duke University. 
+      I am passionate about applying my strong background in biomedical research and expertise 
+      in the design process to create patient-driven innovations with impactful clinical 
+      applications. My goal is to contribute to a healthcare landscape that is not only more 
+      effective but also more inclusive, using technology as a tool to drive health equity 
+      and ensure that high-quality care is accessible to all.
+    </p>
+  </div>
+  <div class="about d-flex align-items-center justify-content-between gap-2 p-4 rounded-5 shadow">
+    <img src="../assets/portrait.jpg" alt="Headshot" class="portrait rounded-4 border"></img>
+    <p class="text">
+      &nbsp;&nbsp;&nbsp;&nbsp;Hi! I'm Connor, a bioengineer with a B.S. in Bioengineering 
+      from UC Riverside, currently pursuing an M.Eng in Med Tech Design at Duke University. 
+      I am passionate about applying my strong background in biomedical research and expertise 
+      in the design process to create patient-driven innovations with impactful clinical 
+      applications. My goal is to contribute to a healthcare landscape that is not only more 
+      effective but also more inclusive, using technology as a tool to drive health equity 
+      and ensure that high-quality care is accessible to all.
+    </p>
+  </div>
+  <div class="about d-flex align-items-center justify-content-between gap-2 p-4 rounded-5 shadow">
+    <img src="../assets/portrait.jpg" alt="Headshot" class="portrait rounded-4 border"></img>
+    <p class="text">
+      &nbsp;&nbsp;&nbsp;&nbsp;Hi! I'm Connor, a bioengineer with a B.S. in Bioengineering 
+      from UC Riverside, currently pursuing an M.Eng in Med Tech Design at Duke University. 
+      I am passionate about applying my strong background in biomedical research and expertise 
+      in the design process to create patient-driven innovations with impactful clinical 
+      applications. My goal is to contribute to a healthcare landscape that is not only more 
+      effective but also more inclusive, using technology as a tool to drive health equity 
+      and ensure that high-quality care is accessible to all.
+    </p>
+  </div>
+  <div class="about d-flex align-items-center justify-content-between gap-2 p-4 rounded-5 shadow">
+    <img src="../assets/portrait.jpg" alt="Headshot" class="portrait rounded-4 border"></img>
+    <p class="text">
+      &nbsp;&nbsp;&nbsp;&nbsp;Hi! I'm Connor, a bioengineer with a B.S. in Bioengineering 
+      from UC Riverside, currently pursuing an M.Eng in Med Tech Design at Duke University. 
+      I am passionate about applying my strong background in biomedical research and expertise 
+      in the design process to create patient-driven innovations with impactful clinical 
+      applications. My goal is to contribute to a healthcare landscape that is not only more 
+      effective but also more inclusive, using technology as a tool to drive health equity 
+      and ensure that high-quality care is accessible to all.
+    </p>
+  </div>
+  <div class="about d-flex align-items-center justify-content-between gap-2 p-4 rounded-5 shadow">
+    <img src="../assets/portrait.jpg" alt="Headshot" class="portrait rounded-4 border"></img>
+    <p class="text">
+      &nbsp;&nbsp;&nbsp;&nbsp;Hi! I'm Connor, a bioengineer with a B.S. in Bioengineering 
+      from UC Riverside, currently pursuing an M.Eng in Med Tech Design at Duke University. 
+      I am passionate about applying my strong background in biomedical research and expertise 
+      in the design process to create patient-driven innovations with impactful clinical 
+      applications. My goal is to contribute to a healthcare landscape that is not only more 
+      effective but also more inclusive, using technology as a tool to drive health equity 
+      and ensure that high-quality care is accessible to all.
+    </p>
+  </div>
+  <div class="about d-flex align-items-center justify-content-between gap-2 p-4 rounded-5 shadow">
+    <img src="../assets/portrait.jpg" alt="Headshot" class="portrait rounded-4 border"></img>
+    <p class="text">
+      &nbsp;&nbsp;&nbsp;&nbsp;Hi! I'm Connor, a bioengineer with a B.S. in Bioengineering 
+      from UC Riverside, currently pursuing an M.Eng in Med Tech Design at Duke University. 
+      I am passionate about applying my strong background in biomedical research and expertise 
+      in the design process to create patient-driven innovations with impactful clinical 
+      applications. My goal is to contribute to a healthcare landscape that is not only more 
+      effective but also more inclusive, using technology as a tool to drive health equity 
+      and ensure that high-quality care is accessible to all.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+  .portrait {
+    width: 40%;
+  }
+
+  .text {
+    text-align: justify;
+    padding: 1rem
+  }
+
+  @media (min-width: 1000px) {
+    .portrait {
+      width: 30%;
+    }
+    .text {
+      font-size: 1.2rem;
+      font-weight: 300;
+    }
+  }
+
+  @media (min-width: 1400px) {
+    .portrait {
+      width: 20%;
+    }
+  }
+
+  @media (max-width: 800px) {
+    .about {
+      flex-direction: column;
+    }
+    .portrait {
+      width: 75%;
+    }
+  }
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+import './styles/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-
 createApp(App).mount('#app')
